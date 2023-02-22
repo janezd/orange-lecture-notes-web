35483c2a960dc6e5f4ca02320aa2dd8e1d245285
mitjapotocin <mitja.potocin@gmail.com>
2023-02-22 18:43:29 +0100

    Add suport for katex

--- pages/books/[slug].js
+++ pages/books/[slug].js
@@ -1,16 +1,19 @@
-import fs from 'fs';
-import React from 'react';
-import matter from 'gray-matter';
-import md from 'markdown-it';
-import Image from '../../components/Image';
-import { ImShrink2, ImEnlarge2 } from 'react-icons/im';
-import Link from 'next/link';
-import { useState } from 'react';
-import { isAllowedPath, parseMd } from '../../utils/helpers';
+import fs from "fs";
+import React from "react";
+import matter from "gray-matter";
+import Image from "../../components/Image";
+import { ImShrink2, ImEnlarge2 } from "react-icons/im";
+import Link from "next/link";
+import { useState } from "react";
+import { isAllowedPath, parseMd } from "../../utils/helpers";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export async function getStaticPaths() {
   const paths = fs
-    .readdirSync('public/books/')
+    .readdirSync("public/books/")
     .filter(isAllowedPath)
     .map((slug) => ({ params: { slug } }));
 
@@ -21,32 +24,43 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const fileName = fs.readFileSync(`public/books/${slug}/index.md`, 'utf-8');
+  const fileName = fs.readFileSync(`public/books/${slug}/index.md`, "utf-8");
   const { data: frontmatter, content } = matter(fileName);
+  const mdxSource = await serialize(parseMd(content, `/books/${slug}`), {
+    mdxOptions: {
+      rehypePlugins: [rehypeKatex],
+    },
+  });
 
-  const chapters = (frontmatter.chapters || [])
-    .map((slug) => {
-      let fileName;
+  const chapters = [];
 
-      try {
-        fileName = fs.readFileSync(`public/chapters/${slug}/index.md`, 'utf-8');
-      } catch (err) {
-        return undefined;
-      }
+  (frontmatter.chapters || []).forEach(async (slug) => {
+    let fileName;
 
-      const { data: frontmatter, content } = matter(fileName);
-      return {
-        frontmatter,
-        content,
-        slug,
-      };
-    })
-    .filter(Boolean);
+    try {
+      fileName = fs.readFileSync(`public/chapters/${slug}/index.md`, "utf-8");
+    } catch (err) {
+      return;
+    }
+
+    const { data: frontmatter, content } = matter(fileName);
+    const mdxSource = await serialize(parseMd(content, `/chapters/${slug}`), {
+      mdxOptions: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+      },
+    });
+
+    chapters.push({
+      frontmatter,
+      content: mdxSource,
+    });
+  });
 
   return {
     props: {
       frontmatter,
-      content,
+      content: mdxSource,
       chapters,
       slug,
     },
@@ -54,7 +68,7 @@ export async function getStaticProps({ params: { slug } }) {
 }
 
 const getTitleId = (title) => {
-  return title.replace(/\s/g, '-').toLowerCase();
+  return title.replace(/\s/g, "-").toLowerCase();
 };
 
 function useOnScreen(ref) {
@@ -75,15 +89,7 @@ function useOnScreen(ref) {
   return isIntersecting;
 }
 
-const Chapter = ({
-  frontmatter,
-  content,
-  index,
-  slug,
-  setIsChapterIndexVisible,
-}) => {
-  let parsedMd = parseMd(content, `/chapters/${slug}`);
-
+const Chapter = ({ frontmatter, content, index, setIsChapterIndexVisible }) => {
   const ref = React.useRef();
   const isVisible = useOnScreen(ref);
 
@@ -92,17 +98,13 @@ const Chapter = ({
   }, [isVisible, setIsChapterIndexVisible, index]);
 
   return (
-    <div ref={ref} className='flex-container'>
-      <div className='right-column'>
-        <div className='prose mx-auto mt-8 chapter'>
-          <h2 className='chapter-title' id={getTitleId(frontmatter.title)}>
+    <div ref={ref} className="flex-container">
+      <div className="right-column">
+        <div className="prose mx-auto mt-8 chapter">
+          <h2 className="chapter-title" id={getTitleId(frontmatter.title)}>
             Chapter {index + 1}: {frontmatter.title}
           </h2>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: md({ html: true }).render(parsedMd),
-            }}
-          />
+          <MDXRemote {...content} />
         </div>
       </div>
     </div>
@@ -125,9 +127,9 @@ const ContentIndex = ({ chapters, isChapterIndexVisible }) => {
   }, [isChapterIndexVisible]);
 
   return (
-    <div className={small ? 'small content-index' : 'content-index'}>
-      <div className='toolbar'>
-        <button className='icon-button' onClick={() => setSmall(!small)}>
+    <div className={small ? "small content-index" : "content-index"}>
+      <div className="toolbar">
+        <button className="icon-button" onClick={() => setSmall(!small)}>
           {small ? <ImEnlarge2 /> : <ImShrink2 />}
         </button>
       </div>
@@ -137,8 +139,8 @@ const ContentIndex = ({ chapters, isChapterIndexVisible }) => {
       <ul>
         {chapters.map(({ frontmatter }, index) => (
           <li key={index}>
-            <Link href={'#' + getTitleId(frontmatter.title)}>
-              <a className={highestVisibleIndex === index ? 'active' : ''}>
+            <Link href={"#" + getTitleId(frontmatter.title)}>
+              <a className={highestVisibleIndex === index ? "active" : ""}>
                 {frontmatter.title}
               </a>
             </Link>
@@ -153,44 +155,36 @@ export default function PostPage({ frontmatter, content, chapters, slug }) {
   const [isChapterIndexVisible, setIsChapterIndexVisible] = useState({});
 
   let relativePath = `/books/${slug}`;
-  let parsedMd = parseMd(content, relativePath);
 
   return (
-    <div className='prose mx-auto book'>
+    <div className="prose mx-auto book">
       {frontmatter.coverImg && (
-        <div className='book-cover-img'>
+        <div className="book-cover-img">
           <Image
             width={650}
             height={650}
-            layout={'responsive'}
-            alt={'cover image'}
+            layout={"responsive"}
+            alt={"cover image"}
             src={`${relativePath}/${frontmatter.coverImg}`}
           />
         </div>
       )}
 
-      <h1 className='max-w-sm mb-0 font-medium'>{frontmatter.title}</h1>
-      <p className='subtitle'>{frontmatter.subTitle}</p>
-      <div className='flex-container'>
-        <div
-          className='right-column'
-          dangerouslySetInnerHTML={{
-            __html: md({ html: true }).render(parsedMd),
-          }}
-        />
-      </div>
+      <h1 className="max-w-sm mb-0 font-medium">{frontmatter.title}</h1>
+      <p className="subtitle">{frontmatter.subTitle}</p>
+
+      <MDXRemote {...content} />
 
       <ContentIndex
         chapters={chapters}
         isChapterIndexVisible={isChapterIndexVisible}
       />
 
-      {chapters.map(({ frontmatter, content, slug }, index) => (
+      {chapters.map(({ frontmatter, content }, index) => (
         <Chapter
           key={frontmatter.title}
           frontmatter={frontmatter}
           content={content}
-          slug={slug}
           index={index}
           setIsChapterIndexVisible={setIsChapterIndexVisible}
         />
